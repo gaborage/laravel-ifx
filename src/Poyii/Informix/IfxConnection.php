d6a7f8071b00b17551c433cf15d305e32d92590c
Gabriel Rosales <gabo.rage@gmail.com>
2021-01-13 21:17:35 -0600

    Update code base to work with laravel 7+

--- src/Poyii/Informix/IfxConnection.php
+++ src/Poyii/Informix/IfxConnection.php
@@ -1,24 +1,23 @@
 <?php
 
 namespace Poyii\Informix;
-/**
+
+/*
  * Created by PhpStorm.
  * User: llaijiale
  * Date: 2016/1/20
  * Time: 14:34
  */
-
+use DateTimeInterface;
 use Illuminate\Database\Connection;
-use Illuminate\Database\Eloquent\Model;
-use Poyii\Informix\Query\Processors\IfxProcessor;
+use Illuminate\Support\Facades\Log;
 use Poyii\Informix\Query\Grammars\IfxGrammar as QueryGrammar;
+use Poyii\Informix\Query\Processors\IfxProcessor;
 use Poyii\Informix\Schema\Grammars\IfxGrammar as SchemaGrammar;
 use Poyii\Informix\Schema\IfxBuilder as SchemaBuilder;
-use DateTimeInterface;
 
 class IfxConnection extends Connection
 {
-
     /**
      * Get a schema builder instance for the connection.
      *
@@ -29,26 +28,15 @@ class IfxConnection extends Connection
         if (is_null($this->schemaGrammar)) {
             $this->useDefaultSchemaGrammar();
         }
+
         return new SchemaBuilder($this);
     }
-
-
-    /**
-     * Get the default post processor instance.
-     *
-     * @return \Illuminate\Database\Query\Processors\SqlServerProcessor
-     */
-    protected function getDefaultPostProcessor()
-    {
-        return new IfxProcessor;
-    }
-
 
     public function prepareBindings(array $bindings)
     {
         $grammar = $this->getQueryGrammar();
         if ($this->isTransEncoding()) {
-            $db_encoding = $this->getConfig('db_encoding');
+            $db_encoding     = $this->getConfig('db_encoding');
             $client_encoding = $this->getConfig('client_encoding');
             foreach ($bindings as $key => &$value) {
                 // We need to transform all instances of DateTimeInterface into the actual
@@ -56,7 +44,7 @@ class IfxConnection extends Connection
                 // so we'll just ask the grammar for the format to get from the date.
                 if ($value instanceof DateTimeInterface) {
                     $value = $value->format($grammar->getDateFormat());
-                } elseif ($value === false) {
+                } elseif (false === $value) {
                     $value = 0;
                 }
                 if (is_string($value)) {
@@ -67,95 +55,51 @@ class IfxConnection extends Connection
             foreach ($bindings as $key => &$value) {
                 if ($value instanceof DateTimeInterface) {
                     $value = $value->format($grammar->getDateFormat());
-                } elseif ($value === false) {
+                } elseif (false === $value) {
                     $value = 0;
                 }
             }
         }
+
         return $bindings;
-    }
-
-    protected function isTransEncoding()
-    {
-        $db_encoding = $this->getConfig('db_encoding');
-        $client_encoding = $this->getConfig('client_encoding');
-        return ($db_encoding && $client_encoding && ($db_encoding != $client_encoding));
-    }
-
-    protected function convertCharset($in_encoding, $out_encoding, $value)
-    {
-
-        //IGNORE
-//        $encoding = mb_detect_encoding($value, mb_detect_order(), false);
-//
-//        if($encoding == $out_encoding)
-//        {
-//            return $value;
-//        }
-//        \Log::debug("encoding: ".$in_encoding." value ".$value);
-        //return mb_convert_encoding(trim($value), $out_encoding);
-        return iconv($in_encoding, "{$out_encoding}//IGNORE", trim($value));
     }
 
     public function select($query, $bindings = [], $useReadPdo = true)
     {
+        if (config('app.debug')) {
+            Log::debug('query: ' . $query . ' with ' . implode(', ', $bindings));
+        }
         $results = parent::select($query, $bindings, $useReadPdo);
         if ($this->isTransEncoding()) {
             if ($results) {
-                $db_encoding = $this->getConfig('db_encoding');
+                $db_encoding     = $this->getConfig('db_encoding');
                 $client_encoding = $this->getConfig('client_encoding');
                 if (is_array($results) || is_object($results)) {
                     foreach ($results as &$result) {
-                        if (is_subclass_of($result, Model::class)) {
-                            $attributes = $result->getAttributes();
-                            foreach ($attributes as $key => $value) {
-                                if (is_string($value)) {
-                                    $value = $this->convertCharset($db_encoding, $client_encoding, $value);
-                                    $result->$key = $value;
-                                    $result->syncOriginalAttribute($key);
-                                }
-                            }
-                        } else if (is_array($result) || is_object($result)) {
+                        if (is_array($result) || is_object($result)) {
                             foreach ($result as $key => &$value) {
                                 if (is_string($value)) {
                                     $value = $this->convertCharset($db_encoding, $client_encoding, $value);
                                 }
                             }
-                        } else if (is_string($result)) {
+                        } elseif (is_string($result)) {
                             $result = $this->convertCharset($db_encoding, $client_encoding, $result);
                         }
                     }
-                } else if (is_string($results)) {
+                } elseif (is_string($results)) {
                     $results = $this->convertCharset($db_encoding, $client_encoding, $results);
                 }
             }
         }
+
         return $results;
     }
 
-    /**
-     * Get the default query grammar instance.
-     *
-     * @return \Illuminate\Database\Query\Grammars\SqlServerGrammar
-     */
-    protected function getDefaultQueryGrammar()
-    {
-        return $this->withTablePrefix(new QueryGrammar);
-    }
-
-    /**
-     * Get the default schema grammar instance.
-     *
-     * @return \Illuminate\Database\Schema\Grammars\SqlServerGrammar
-     */
-    protected function getDefaultSchemaGrammar()
-    {
-        return $this->withTablePrefix(new SchemaGrammar);
-    }
-
-
     public function statement($query, $bindings = [])
     {
+        if (config('app.debug')) {
+            Log::debug('statement: ' . $query . ' with ' . implode(', ', $bindings));
+        }
 
         return $this->run($query, $bindings, function ($query, $bindings) {
             if ($this->pretending()) {
@@ -164,12 +108,13 @@ class IfxConnection extends Connection
             $count = substr_count($query, '?');
             if ($count == count($bindings)) {
                 $bindings = $this->prepareBindings($bindings);
+
                 return $this->getPdo()->prepare($query)->execute($bindings);
             }
 
-            if (count($bindings) % $count > 0)
+            if (count($bindings) % $count > 0) {
                 throw new \InvalidArgumentException('the driver can not support multi-insert.');
-
+            }
             $mutiBindings = array_chunk($bindings, $count);
             $this->beginTransaction();
             try {
@@ -182,20 +127,77 @@ class IfxConnection extends Connection
                 }
             } catch (\Exception $e) {
                 $this->rollBack();
+
                 return false;
             } catch (\Throwable $e) {
                 $this->rollBack();
+
                 return false;
             }
             $this->commit();
 
             return true;
-
         });
     }
 
-    public function affectingStatement($query, $bindings = []): int
+    public function affectingStatement($query, $bindings = [])
     {
+        if (config('app.debug')) {
+            Log::debug('affectingStatement: ' . $query . ' with ' . implode(', ', $bindings));
+        }
+
         return parent::affectingStatement($query, $bindings);
+    }
+
+    /**
+     * Get the default post processor instance.
+     *
+     * @return \Illuminate\Database\Query\Processors\SqlServerProcessor
+     */
+    protected function getDefaultPostProcessor()
+    {
+        return new IfxProcessor();
+    }
+
+    protected function isTransEncoding()
+    {
+        $db_encoding     = $this->getConfig('db_encoding');
+        $client_encoding = $this->getConfig('client_encoding');
+
+        return $db_encoding && $client_encoding && ($db_encoding != $client_encoding);
+    }
+
+    protected function convertCharset($in_encoding, $out_encoding, $value)
+    {
+        //IGNORE
+//        $encoding = mb_detect_encoding($value, mb_detect_order(), false);
+//
+//        if($encoding == $out_encoding)
+//        {
+//            return $value;
+//        }
+//        Log::debug("encoding: ".$in_encoding." value ".$value);
+        //return mb_convert_encoding(trim($value), $out_encoding);
+        return iconv($in_encoding, "{$out_encoding}//IGNORE", trim($value));
+    }
+
+    /**
+     * Get the default query grammar instance.
+     *
+     * @return \Illuminate\Database\Query\Grammars\SqlServerGrammar
+     */
+    protected function getDefaultQueryGrammar()
+    {
+        return $this->withTablePrefix(new QueryGrammar());
+    }
+
+    /**
+     * Get the default schema grammar instance.
+     *
+     * @return \Illuminate\Database\Schema\Grammars\SqlServerGrammar
+     */
+    protected function getDefaultSchemaGrammar()
+    {
+        return $this->withTablePrefix(new SchemaGrammar());
     }
 }
--- src/Poyii/Informix/InformixDBServiceProvider.php
+++ src/Poyii/Informix/InformixDBServiceProvider.php
@@ -3,29 +3,24 @@
  * Created by PhpStorm.
  * User: llaijiale
  * Date: 2017/8/2
- * Time: 10:40
+ * Time: 10:40.
  */
 
 namespace Poyii\Informix;
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
+
 /**
  * Class InformixDBServiceProvider.
  */
 class InformixDBServiceProvider extends ServiceProvider
 {
-
     /**
      * Boot.
      */
     public function boot()
     {
-        $this->publishes(
-            [
-                __DIR__.'/../../config/informix.php' => config_path('informix.php'),
-            ]
-        );
+        
     }
 
     /**
@@ -36,7 +31,6 @@ class InformixDBServiceProvider extends ServiceProvider
     public function register()
     {
         if (file_exists(config_path('informix.php'))) {
-
             $this->mergeConfigFrom(config_path('informix.php'), 'database.connections');
 
             $config = $this->app['config']->get('informix', []);
@@ -45,14 +39,10 @@ class InformixDBServiceProvider extends ServiceProvider
 
             foreach ($connection_keys as $key) {
                 $this->app['db']->extend($key, function ($config) {
-                    $driver = Arr::get($config, "driver", "informix");
-                    if($driver === "informix"){
-                        $oConnector = new Connectors\IfxConnector($this->app['encrypter']);
-                        $connection = $oConnector->connect($config);
-                        return new IfxConnection($connection, $config['database'], $config['prefix'], $config);
-                    } else if($driver === "informix-json") {
-                        return new IfxJsonConnection($config);
-                    }
+                    $oConnector = new Connectors\IfxConnector($this->app['encrypter']);
+                    $connection = $oConnector->connect($config);
+
+                    return new IfxConnection($connection, $config['database'], $config['prefix'], $config);
                 });
             }
         }
